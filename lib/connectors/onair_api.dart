0d3cebc1bf7355225763eff580550299620fbac6
Raphaël Santos <ra@astons.me>
2021-07-20 12:54:47 +0200

    Fetch data from OnAir

--- lib/connectors/onair_api.dart
+++ lib/connectors/onair_api.dart
@@ -2,24 +2,53 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import '../models/aircraft.dart';
+import '../models/job.dart';
+
+class OnAirApiException implements Exception {
+  final dynamic message;
+
+  OnAirApiException([this.message]);
+}
+
 class OnAirApi {
   final String _oaApiKey;
   final String _companyId;
 
   OnAirApi(this._oaApiKey, this._companyId);
 
-  Future<bool> validateCredentials() async {
-    var validCreds = false;
-    final response = await http.get(
-      Uri.parse('https://thunder.onair.company/api/v1/company/$_companyId'),
-      headers: {'oa-apikey': _oaApiKey},
-    );
+  Future<Map<String, dynamic>> _get(String url) async {
+    final uri = Uri.parse(url);
+    final response = await http.get(uri, headers: {'oa-apikey': _oaApiKey});
+    if (response.statusCode == 404) throw OnAirApiException("Invalid endpoint");
     if (response.statusCode == 200) {
-      final json = (jsonDecode(response.body) as Map<String, dynamic>);
-      if (!json.containsKey('Error')) {
-        validCreds = true;
-      }
+      final json = jsonDecode(response.body) as Map<String, dynamic>;
+      if (json.containsKey("Error")) throw OnAirApiException(json["Error"]);
+      return json;
+    } else {
+      throw OnAirApiException("API returned ${response.statusCode}");
     }
-    return validCreds;
+  }
+
+  Future<bool> validateCredentials() async {
+    try {
+      final url = 'https://thunder.onair.company/api/v1/company/$_companyId';
+      await _get(url);
+      return true;
+    } on OnAirApiException {
+      return false;
+    }
+  }
+
+  Future<List<Aircraft>> fetchFleet() async {
+    final url = 'https://thunder.onair.company/api/v1/company/$_companyId/fleet';
+    final json = await _get(url);
+    return (json['Content'] as List<dynamic>).map((j) => Aircraft.fromJson(j)).toList();
+  }
+
+  Future<List<Job>> fetchPendingJobs() async {
+    final url = 'https://thunder.onair.company/api/v1/company/$_companyId/jobs/pending';
+    final json = await _get(url);
+    return (json['Content'] as List<dynamic>).map((j) => Job.fromJson(j)).toList();
   }
 }
--- lib/screens/planner_screen.dart
+++ lib/screens/planner_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../connectors/onair_api.dart';
+import '../models/fleet.dart';
+import '../models/pending_jobs.dart';
+import '../models/settings.dart';
 import '../widgets/app_drawer.dart';
 
 class PlannerScreen extends StatefulWidget {
@@ -13,6 +18,26 @@ class PlannerScreen extends StatefulWidget {
 }
 
 class _PlannerScreenState extends State<PlannerScreen> {
+  late Fleet _fleet;
+  late PendingJobs _pendingJobs;
+  var _isInit = true;
+
+  void _fetchData() async {
+    final _settings = Provider.of<Settings>(context);
+    final onAir = OnAirApi(_settings.oaApiKey, _settings.companyId);
+    _fleet.updateAircrafts(await onAir.fetchFleet());
+    _pendingJobs.updateJobs(await onAir.fetchPendingJobs());
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      _fetchData();
+      _isInit = false;
+    }
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
