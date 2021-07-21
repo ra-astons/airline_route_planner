d549697aba4a2ca14e8ded73b3e7b7b6e1499b69
Raphaël Santos <ra@astons.me>
2021-07-21 12:06:54 +0200

    Improve map readability

--- lib/helpers/map_marker.dart
+++ lib/helpers/map_marker.dart
@@ -3,51 +3,85 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 import '../models/job_leg.dart';
-import '../models/airport.dart';
 
-enum MarkerTypes {
-  departure,
-  destination,
-}
+class JobLegMarkers {
+  final JobLeg _jobLeg;
+  late LatLng _departurePoint;
+  late LatLng _destinationPoint;
+  late LatLng _middlePoint;
 
-Marker buildMarker(Airport airport, MarkerTypes markerType) {
-  var color = Colors.green;
-  if (markerType == MarkerTypes.destination) color = Colors.red;
-  return Marker(
-    height: 62,
-    width: 62,
-    point: LatLng(airport.latitude, airport.longitude),
-    anchorPos: AnchorPos.align(AnchorAlign.top),
-    builder: (ctx) => Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text(airport.icao, style: TextStyle(color: color)),
-          Icon(Icons.location_pin, color: color),
-        ],
+  JobLegMarkers(this._jobLeg) {
+    _departurePoint = LatLng(_jobLeg.departureAirport.latitude, _jobLeg.departureAirport.longitude);
+    _destinationPoint = LatLng(_jobLeg.destinationAirport.latitude, _jobLeg.destinationAirport.longitude);
+    final path = Path.from([_departurePoint, _destinationPoint]);
+    _middlePoint = path.center;
+  }
+
+  Marker get departureMarker {
+    return _buildAirportMarker(_departurePoint, Colors.red, _jobLeg.departureAirport.icao);
+  }
+
+  Marker get destinationMarker {
+    return _buildAirportMarker(_destinationPoint, Colors.green, _jobLeg.destinationAirport.icao);
+  }
+
+  Marker get middleMarker {
+    var icon = Icon(Icons.archive, size: 20, color: Colors.white);
+    var tooltipText = _jobLeg.weight.toString();
+    if (_jobLeg.passengers > 0) {
+      icon = Icon(Icons.group, size: 20, color: Colors.white);
+      tooltipText = _jobLeg.passengers.toString();
+    }
+    return Marker(
+      point: _middlePoint,
+      height: 30,
+      width: 30,
+      anchorPos: AnchorPos.align(AnchorAlign.center),
+      builder: (ctx) => Container(
+        child: Tooltip(
+          message: tooltipText,
+          textStyle: TextStyle(color: Colors.white),
+          preferBelow: false,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.red),
+          child: CircleAvatar(
+            child: icon,
+            backgroundColor: Colors.red,
+          ),
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Polyline buildPolyline(JobLeg leg) {
-  return Polyline(
-    strokeWidth: 4,
-    colorsStop: [0.0, 1.0],
-    gradientColors: [
-      Colors.green,
-      Colors.red,
-    ],
-    isDotted: true,
-    points: [
-      LatLng(
-        leg.departureAirport.latitude,
-        leg.departureAirport.longitude,
+  Polyline get pathLine {
+    final path = Path.from([_departurePoint, _middlePoint, _destinationPoint]).equalize(10000, smoothPath: true);
+    return Polyline(
+      strokeWidth: 3,
+      colorsStop: [0.0, 1.0],
+      gradientColors: [
+        Colors.red,
+        Colors.green,
+      ],
+      points: path.coordinates,
+    );
+  }
+
+  Marker _buildAirportMarker(LatLng point, Color color, String text) {
+    return Marker(
+      height: 40,
+      width: 40,
+      point: point,
+      anchorPos: AnchorPos.align(AnchorAlign.top),
+      builder: (ctx) => Container(
+        child: Tooltip(
+          message: text,
+          textStyle: TextStyle(color: Colors.white),
+          preferBelow: false,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: color),
+          child: Icon(Icons.location_pin, size: 40, color: color),
+        ),
       ),
-      LatLng(
-        leg.destinationAirport.latitude,
-        leg.destinationAirport.longitude,
-      )
-    ],
-  );
+    );
+  }
 }
--- lib/widgets/planner_map.dart
+++ lib/widgets/planner_map.dart
@@ -13,29 +13,31 @@ class PlannerMap extends StatefulWidget {
 
 class _PlannerMapState extends State<PlannerMap> {
   final _mapController = MapController();
+  final _fitBoundOptions = FitBoundsOptions(padding: EdgeInsets.all(50));
   late PendingJobs _pendingJobs;
   var _isInit = true;
-  LatLngBounds _bounds = LatLngBounds.fromPoints([LatLng(90.0, -180.0), LatLng(-90.0, 180.0)]);
   List<Polyline> _polylines = [];
   List<Marker> _departureMarkers = [];
   List<Marker> _destinationMarkers = [];
+  List<Marker> _allMarkers = [];
+  List<Marker> _middleMarkers = [];
 
   LatLngBounds _buildBounds() {
-    if (_departureMarkers.isEmpty && _destinationMarkers.isEmpty) {
-      return LatLngBounds.fromPoints([LatLng(90.0, -180.0), LatLng(-90.0, 180.0)]);
-    } else {
-      final markers = [
-        ..._departureMarkers,
-        ..._destinationMarkers,
-      ];
-      markers.sort((a, b) => b.point.latitude.compareTo(a.point.latitude));
-      final maxLatitude = markers.first.point.latitude + 0.5;
-      final minLatitude = markers.last.point.latitude - 0.5;
-      markers.sort((a, b) => b.point.longitude.compareTo(a.point.longitude));
-      final maxLongitude = markers.first.point.longitude + 0.5;
-      final minLongitude = markers.last.point.longitude - 0.5;
-      return LatLngBounds.fromPoints([LatLng(minLatitude, minLongitude), LatLng(maxLatitude, maxLongitude)]);
-    }
+    final allLatLngs = _allMarkers.map((e) => e.point).toList();
+    return LatLngBounds.fromPoints(allLatLngs);
+  }
+
+  void _buildMarkers() {
+    final legs = _pendingJobs.jobs.map((e) => e.selectedLegs).expand((e) => e).toList();
+    _departureMarkers = _destinationMarkers = _middleMarkers = [];
+    final legMarkers = legs.map((l) => JobLegMarkers(l));
+    legMarkers.forEach((legMarker) {
+      _departureMarkers.add(legMarker.departureMarker);
+      _destinationMarkers.add(legMarker.destinationMarker);
+      _middleMarkers.add(legMarker.middleMarker);
+      _polylines.add(legMarker.pathLine);
+    });
+    _allMarkers = [..._departureMarkers, ..._destinationMarkers];
   }
 
   @override
@@ -45,31 +47,14 @@ class _PlannerMapState extends State<PlannerMap> {
       _isInit = false;
       _pendingJobs.addListener(() {
         setState(() {
-          _departureMarkers = _getDepartureMarkers();
-          _destinationMarkers = _getDestinationMarkers();
-          _polylines = _getPolylines();
-          _mapController.fitBounds(_buildBounds());
+          _buildMarkers();
+          if (_allMarkers.isNotEmpty) {
+            _mapController.fitBounds(_buildBounds(), options: _fitBoundOptions);
+          }
         });
       });
     }
     super.didChangeDependencies();
-  }
-
-  List<Marker> _getDepartureMarkers() {
-    final legs = _pendingJobs.jobs.map((e) => e.selectedLegs).expand((e) => e).toList();
-    _departureMarkers = legs.map((l) => buildMarker(l.departureAirport, MarkerTypes.departure)).toList();
-    return _departureMarkers;
-  }
-
-  List<Marker> _getDestinationMarkers() {
-    final legs = _pendingJobs.jobs.map((e) => e.selectedLegs).expand((e) => e).toList();
-    _destinationMarkers = legs.map((l) => buildMarker(l.destinationAirport, MarkerTypes.destination)).toList();
-    return _destinationMarkers;
-  }
-
-  List<Polyline> _getPolylines() {
-    final legs = _pendingJobs.jobs.map((e) => e.selectedLegs).expand((e) => e).toList();
-    return legs.map((l) => buildPolyline(l)).toList();
   }
 
   @override
@@ -81,7 +66,7 @@ class _PlannerMapState extends State<PlannerMap> {
           child: FlutterMap(
             mapController: _mapController,
             options: MapOptions(
-              bounds: _bounds,
+              bounds: LatLngBounds.fromPoints([LatLng(90.0, -180.0), LatLng(-90.0, 180.0)]),
               center: LatLng(0, 0),
               zoom: 1.0,
             ),
@@ -95,6 +80,11 @@ class _PlannerMapState extends State<PlannerMap> {
               PolylineLayerWidget(
                 options: PolylineLayerOptions(
                   polylines: _polylines,
+                ),
+              ),
+              MarkerLayerWidget(
+                options: MarkerLayerOptions(
+                  markers: _middleMarkers,
                 ),
               ),
               MarkerLayerWidget(
